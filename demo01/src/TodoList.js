9da8ed29a255481b09ef0b7dc5fda6fe74a6ab01
timo <410379528@qq.com>
2022-01-13 10:41:45 +0800

    1. 增加、删除使用 2. 使用actionType公共文件管理action

--- demo01/src/TodoList.js
+++ demo01/src/TodoList.js
@@ -2,14 +2,45 @@ import React, { Component } from "react";
 import "antd/dist/antd.css";
 import { Input, Button, List } from "antd";
 import store from "./store";
+import { CHANGE_INPUT, ADD_ITEM, DEL_ITEM } from "./store/actionType.js";
 
-// const data = ["1. 起床", "2. 洗漱", "3. 吃饭", "4. 上班"];
 class TodoList extends Component {
   constructor(props) {
     super(props);
-    console.log(store.getState());
-    this.state = store.getState()
+    this.state = store.getState();
+    this.storeChange = this.storeChange.bind(this);
+    store.subscribe(this.storeChange);
   }
+
+  // 更新状态
+  storeChange() {
+    this.setState(store.getState());
+  }
+
+  changeInputValue = (e) => {
+    const action = {
+      type: CHANGE_INPUT,
+      value: e.target.value,
+    };
+    store.dispatch(action);
+  };
+
+  clickBtn = () => {
+    const action = {
+      type: ADD_ITEM,
+    };
+    store.dispatch(action);
+  };
+
+  clickBtnDel(index) {
+    console.log(index);
+    const action = {
+      type: DEL_ITEM,
+      index,
+    };
+    store.dispatch(action);
+  }
+
   render() {
     return (
       <div style={{ margin: "10px" }}>
@@ -17,14 +48,29 @@ class TodoList extends Component {
           <Input
             placeholder={this.state.inputValue}
             style={{ width: "250px", marginRight: "10px" }}
+            onChange={this.changeInputValue}
+            value={this.state.inputValue}
           />
-          <Button type="primary">增加</Button>
+          <Button type="primary" onClick={this.clickBtn}>
+            增加
+          </Button>
         </div>
         <div style={{ margin: " 10px", width: "300px" }}>
           <List
             bordered
             dataSource={this.state.list}
-            renderItem={(item) => <List.Item>{item}</List.Item>}
+            renderItem={(item, index) => (
+              <List.Item onClick={() => this.clickBtnDel(index)}>
+                {item}
+                {/* <Button
+                  style={{ float: "right" }}
+                  type="primary"
+                  onClick={this.clickBtnDel(index)}
+                >
+                  删除
+                </Button> */}
+              </List.Item>
+            )}
           />
         </div>
       </div>
--- demo01/src/store/reducer.js
+++ demo01/src/store/reducer.js
@@ -1,8 +1,29 @@
+import { CHANGE_INPUT, ADD_ITEM, DEL_ITEM } from "./actionType.js";
+
 const defaultState = {
   inputValue: " Write Something",
   list: ["1. 起床", "2. 洗漱", "3. 吃饭", "4. 上班"],
 };
 
 export default (state = defaultState, action) => {
+  // reducer里只能接收state，不能改变state
+  if (action.type === CHANGE_INPUT) {
+    let newState = JSON.parse(JSON.stringify(state));
+    newState.inputValue = action.value;
+    return newState;
+  }
+  if (action.type === ADD_ITEM) {
+    let newState = JSON.parse(JSON.stringify(state));
+    if (newState.inputValue) {
+      newState.list.push(newState.inputValue);
+    }
+    newState.inputValue = "";
+    return newState;
+  }
+  if (action.type === DEL_ITEM) {
+    let newState = JSON.parse(JSON.stringify(state));
+    newState.list.splice(action.index,1);
+    return newState;
+  }
   return state;
 };
